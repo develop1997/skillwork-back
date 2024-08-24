4a005579046b5e0f86bb9482835d9a1231221ab6
develop1997 <colombia.develop@gmail.com>
2024-08-24 15:06:03 -0500

    feat: config

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,21 @@
-import app from "../src/server";
+import dotenv from "dotenv";
+import { createDebugger } from "../src/utils/debugConfig";
+import { App } from "../src/app";
+
+// CONFIGURATION
+dotenv.config();
+const PORT: number = parseInt(process.env.PORT ?? '3000', 10);
+
+// APP
+const app = new App().config();
+
+// DEBUGGER
+const serverDebugger = createDebugger('server');
+
+
+// LISTEN
+app.listen(PORT, () => {
+    serverDebugger(`Listening on port ${PORT}`);
+});
 
 export default app
